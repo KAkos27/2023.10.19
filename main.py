baf45ad1e74d10d8f1da8a98278d9418c947fc8e
Kosztolányi Ákos (SZF_2023_2025) <kosztolanyi.akos@diak.szamalk-szalezi.hu>
2023-10-19 12:03:33 +0200

    Feladatok

---
+++ main.py
@@ -0,0 +1,3 @@
+import szamolasok
+
+szamolasok.hatodik()
---
+++ szamolasok.py
@@ -0,0 +1,70 @@
+def elso():
+    szam: int = int(input("Kérek egy nullánál nagyobb számot: "))
+    while szam <= 0:
+        print("Nullánál nagyobb számot kell megadni!")
+        szam: int = int(input("Kérek egy nullánál nagyobb számot: "))
+    
+    i: int = 1
+    while i <= szam:
+        print(i)
+        i+= 1
+
+def masodik():
+    szam: int = int(input("Kérek egy nullánál nagyobb számot: "))
+    while szam <= 0:
+        print("Nullánál nagyobb számot kell megadni!")
+        szam: int = int(input("Kérek egy nullánál nagyobb számot: "))
+
+    i: int = 1
+    k: int = 0
+    osszeg: int = 0
+    while i <= szam:
+        k = szam % i
+        if k == 0:
+            print(i)
+            osszeg += i
+        i += 1
+    print(f"Összeg: {osszeg}")
+
+def harmadik():
+    szam: int = int(input("Kérek egy számot: "))
+    szam2: int = int(input("Kérek még egy számot: "))
+    while szam2 <= szam:
+        print("Az első számnak kisebbnek kell lennie a másiknál!")
+        szam2: int = int(input("Kérek még egy számot: "))
+    
+    k: int = 0
+    while szam <= szam2:
+        k = szam % 2
+        if k == 0:
+            print(szam)
+        szam+=1
+
+def negyedik():
+    i: int = 1
+
+    while i != 21:
+        print(i*i)
+        i+=1
+
+def otodik():
+    szoveg: str = str(input("Kérek egy tetszőleges szöveget: "))
+    
+    i: int = 0
+    while i < len(szoveg):
+        print(szoveg[i])
+        i+=1
+
+def hatodik():
+    szoveg: str = "Indul a kutya és a tyúk aludni"
+
+    i: int = 0
+    hossz: int  = len(szoveg-1)
+    while i > 0:
+        index: int = hossz - i
+        print(szoveg[index])
+        i -= 1
+    
+    
+
+
